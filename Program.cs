408486630f26cd6025c7d6db5854545dd7ff5ed1
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2025-10-02 12:57:56 +0200

    start

--- Program.cs
+++ Program.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using DecoratorPattern.Beverages;
-using DecoratorPattern.Factory;
-
-namespace DecoratorPattern
+﻿namespace Singleton
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            foreach (name type in Enum.GetValues(typeof(name)))
-            {
-                foreach (Size size in Enum.GetValues(typeof(Size)))
-                {
-                    var beverage = BeverageFactory.CreateBeverage(type, size);
-                    PrintBeverage(beverage);
-                }
-            }
-        }
-
-        public static void PrintBeverage(Beverage beverage)
-        {
-            Console.WriteLine($"{beverage.Size} {beverage.GetDescription()} ${beverage.cost():0.##}");
         }
     }
 }
