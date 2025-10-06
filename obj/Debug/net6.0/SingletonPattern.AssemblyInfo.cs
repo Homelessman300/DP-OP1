8d4c17edadee00af1ab68caf697c667df1443163
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2025-10-06 13:23:07 +0200

    I'm not to sure what going on

--- obj/Debug/net6.0/SingletonPattern.AssemblyInfo.cs
+++ obj/Debug/net6.0/SingletonPattern.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+408486630f26cd6025c7d6db5854545dd7ff5ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
